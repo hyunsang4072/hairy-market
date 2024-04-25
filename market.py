cdc91d44c26d477af0f990421c7fe70852a761a9
hyunsang4072 <hyunsang4072@gmail.com>
2024-04-25 03:32:40 -0700

    add templates

--- market.py
+++ market.py
@@ -1,9 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+# from markupsafe import escape
 
 # create an instance of Flask
 app = Flask(__name__)
 
 # what URL should trigger this function
 @app.route("/") # decorator
+@app.route("/home")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("home.html")
+
+@app.route("/about")
+def about_me():
+    return "<h1>Tell me about yourself!</h1>"
+
+@app.route("/about/<username>")
+def about_user(username):
+    return f"<h1>Hi, there! {username}</h1>"
